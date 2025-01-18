29b51ab7ceff54f59906cbb1d5fc4f440ef2e7b0
Elousseynou <elhadj.ousseynou.fall@umontreal.ca>
2025-01-18 15:48:55 -0500

    "ajouter des couleurs"

---
+++ demogit.sh
@@ -0,0 +1,8 @@
+##Dire git qui vous êtes
+#scrip shell
+git config --global user.name "Elhadji Ousseynou"
+git config --global user.email"elhadj.ousseynou.fall@umontreal.ca"
+##windows
+git config --global core.autocrlf true
+git config --global core.editor"nono-w"
+git config --global init.defaultBranch main
